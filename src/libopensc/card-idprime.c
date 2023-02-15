2cbb409a050d3ae196bce9c5915f24ad8f9dcc73
Veronika Hanulikova <xhanulik@fi.muni.cz>
2023-02-15 11:11:16 +0100

    Remove decompression from IDPrime driver

--- src/libopensc/card-idprime.c
+++ src/libopensc/card-idprime.c
@@ -590,34 +590,12 @@ static int idprime_read_binary(sc_card_t *card, unsigned int offset,
 		if (read < 4 || read != priv->file_size) {
 			LOG_FUNC_RETURN(card->ctx, SC_ERROR_INVALID_DATA);
 		}
-		if (buffer[0] == 1 && buffer[1] == 0) {
-#ifdef ENABLE_ZLIB
-			size_t expectedsize = buffer[2] + buffer[3] * 0x100;
-			r = sc_decompress_alloc(&priv->cache_buf, &(priv->cache_buf_len),
-				buffer+4, priv->file_size-4, COMPRESSION_AUTO);
-			if (r != SC_SUCCESS) {
-				sc_log(card->ctx, "Zlib error: %d", r);
-				LOG_FUNC_RETURN(card->ctx, r);
-			}
-			if (priv->cache_buf_len != expectedsize) {
-				sc_log(card->ctx,
-					 "expected size: %"SC_FORMAT_LEN_SIZE_T"u real size: %"SC_FORMAT_LEN_SIZE_T"u",
-					 expectedsize, priv->cache_buf_len);
-				LOG_FUNC_RETURN(card->ctx, SC_ERROR_INVALID_DATA);
-			}
-#else
-			sc_log(card->ctx, "compression not supported, no zlib");
-			return SC_ERROR_NOT_SUPPORTED;
-#endif /* ENABLE_ZLIB */
-		} else {
-			/* assuming uncompressed certificate */
-			priv->cache_buf = malloc(r);
-			if (priv->cache_buf == NULL) {
-				return SC_ERROR_OUT_OF_MEMORY;
-			}
-			memcpy(priv->cache_buf, buffer, r);
-			priv->cache_buf_len = r;
+		priv->cache_buf = malloc(r);
+		if (priv->cache_buf == NULL) {
+			return SC_ERROR_OUT_OF_MEMORY;
 		}
+		memcpy(priv->cache_buf, buffer, r);
+		priv->cache_buf_len = r;
 		priv->cached = 1;
 	}
 	if (offset >= priv->cache_buf_len) {
