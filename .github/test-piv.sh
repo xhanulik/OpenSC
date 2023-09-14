707f19f63ac6a875518ea240c356f36e9c29b9f1
Veronika Hanulíková <vhanulik@redhat.com>
2023-09-14 13:50:42 +0200

    Run p11test with piv in CI

--- .github/test-piv.sh
+++ .github/test-piv.sh
@@ -57,6 +57,22 @@ yubico-piv-tool -v 9999 -r 'Virtual PCD 00 00' -P "$PIN" -s 9a -a generate -A EC
 yubico-piv-tool -v 9999 -r 'Virtual PCD 00 00' -P "$PIN" -s 9a -S'/CN=bar/OU=test/O=example.com/' -averify -aselfsign < 9a.pub | tee 9a.cert
 yubico-piv-tool -v 9999 -r 'Virtual PCD 00 00' -P "$PIN" -s 9a -aimport-certificate < 9a.cert
 
+yubico-piv-tool -v 9999 -r 'Virtual PCD 00 00' -P "$PIN" -s 9c -a generate -A ECCP256 | tee 9c.pub
+yubico-piv-tool -v 9999 -r 'Virtual PCD 00 00' -P "$PIN" -s 9c -S'/CN=bar/OU=test/O=example.com/' -averify -aselfsign < 9c.pub | tee 9c.cert
+yubico-piv-tool -v 9999 -r 'Virtual PCD 00 00' -P "$PIN" -s 9c -aimport-certificate < 9c.cert
+
+yubico-piv-tool -v 9999 -r 'Virtual PCD 00 00' -P "$PIN" -s 9d -a generate -A RSA2048 | tee 9d.pub
+yubico-piv-tool -v 9999 -r 'Virtual PCD 00 00' -P "$PIN" -s 9d -S'/CN=bar/OU=test/O=example.com/' -averify -aselfsign < 9d.pub | tee 9d.cert
+yubico-piv-tool -v 9999 -r 'Virtual PCD 00 00' -P "$PIN" -s 9d -aimport-certificate < 9d.cert
+
 $VALGRIND pkcs11-tool -l -O -p "$PIN"
 $VALGRIND pkcs11-tool -l -t -p "$PIN"
+
+# run p11test
+pushd src/tests/p11test/
+sleep 5
+$VALGRIND ./p11test -s 0 -p "$PIN" -o piv.json || true
+popd
+diff -u3 src/tests/p11test/piv{_ref,}.json
+
 kill -9 $PID
